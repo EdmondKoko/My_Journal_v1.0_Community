68e60bdcbbcc1fa061321d6c4fc4f89cea19cb83
EdmondKoko <107623627+EdmondKoko@users.noreply.github.com>
2023-10-08 14:27:09 +0300

    Создаем модель Post и Group

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -6,6 +6,7 @@ User = get_user_model()
 
 
 class Group(models.Model):
+    """Создаем модель Group"""
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=50, unique=True)
     description = models.TextField()
@@ -15,6 +16,7 @@ class Group(models.Model):
 
 
 class Post(models.Model):
+    """Создаем модель Post"""
     text = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
