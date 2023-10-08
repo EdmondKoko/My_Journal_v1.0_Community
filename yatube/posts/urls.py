258a7e32b7826e004ff2e9f3da06859abd66f666
EdmondKoko <107623627+EdmondKoko@users.noreply.github.com>
2023-10-08 14:31:22 +0300

    Создаем urls для главной страницы и страницы Group

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 app_name = 'posts'
 
+"""Создаем urls для главной страницы и страницы Group"""
 urlpatterns = [
     path('', views.index, name='index'),
     path('group/<slug>/', views.group_posts, name='group_list'),
